05360ed44a58640ebe7ba6a668386bc3cd94ee65
lumoreira <lulii.moreira96@gmail.com>
2025-06-17 23:45:40 -0300

    agrego componente lista de productos

---
+++ src/app/productos-list/productos-list.component.ts
@@ -0,0 +1,11 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-productos-list',
+  standalone: false,
+  templateUrl: './productos-list.component.html',
+  styleUrl: './productos-list.component.scss'
+})
+export class ProductosListComponent {
+
+}
